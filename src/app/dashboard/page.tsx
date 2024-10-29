2cb2f83af36b377efdffa1c494cccc96a0f668df
Nickothan <s1nicolasrg@gmail.com>
2024-10-28 21:37:31 -0500

    Page dashboard.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,28 @@
-import Link from "next/link";
+import DashboardLayout from "./template";
+import CardPropertyDashboard from "../components/card-property-dashboard";
+import CardAdminDashboard from "../components/card-admin-dashboard";
 
 export default function Dashboard() {
   return (
-    <div>
-      <Link href="/">Go Home</Link>
-    </div>
+    <DashboardLayout>
+      <div className="flex gap-[150px]">
+        <div className="flex flex-col gap-5 p-5 rounded-lg border max-w-[441px]">
+          <CardPropertyDashboard />
+          <CardPropertyDashboard />
+          <CardPropertyDashboard />
+          <CardPropertyDashboard />
+          <CardPropertyDashboard />
+        </div>
+        <div className="flex gap-[150px]">
+          <div className="flex flex-col gap-5 p-5 rounded-lg border max-w-[441px]">
+            <CardAdminDashboard />
+            <CardAdminDashboard />
+            <CardAdminDashboard />
+            <CardAdminDashboard />
+            <CardAdminDashboard />
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
   );
 }
