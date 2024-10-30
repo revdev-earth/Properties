b788b399901070042f0d8a9c0b572099e7614234
Nickothan <s1nicolasrg@gmail.com>
2024-10-30 05:41:24 -0500

    LAS-7: eliminando template de dashboard.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,24 +1,23 @@
-import DashboardLayout from "./template";
-import CardPropertyDashboard from "../components/card-property-dashboard";
-import CardAdminDashboard from "../components/card-admin-dashboard";
+import CardAdmin from "./fragments/cardAdmin";
+import CardProperty from "./fragments/cardProperty";
 
 export default function Dashboard() {
   return (
-    <DashboardLayout>
+    <>
       <div className="flex gap-[150px]">
         <div className="flex flex-col gap-5 p-5 rounded-lg border max-w-[441px]">
           {new Array(5).fill(null).map(() => (
-            <CardPropertyDashboard key={Math.random() + 10} />
+            <CardProperty key={Math.random() + 10} />
           ))}
         </div>
         <div className="flex gap-[150px]">
           <div className="flex flex-col gap-5 p-5 rounded-lg border max-w-[441px]">
             {new Array(5).fill(null).map(() => (
-              <CardAdminDashboard key={Math.random() + 10} />
+              <CardAdmin key={Math.random() + 10} />
             ))}
           </div>
         </div>
       </div>
-    </DashboardLayout>
+    </>
   );
 }
