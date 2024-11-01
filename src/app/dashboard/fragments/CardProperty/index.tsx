fc8f2e1a1d4deda2157aceabda26dfcef6dd94ac
Nickothan <38267333+nickothan@users.noreply.github.com>
2024-11-01 09:57:07 -0500

    LAS-7: dashboard (#2)
    * create page dashboard and add links navigation.
    
    * Page dashboard.
    
    * icons, images and farray from dashboard.
    
    * LAS-7: actualizando template page de app.
    
    * LAS-7: cambiando cartas a a fragments.
    
    * LAS-7: eliminando template de dashboard.
    
    * LAS-7: array and .map from cardProperty.
    
    * LAS-7: remove main from layout
    
    * LAS-7: Add title and prisma db folder to git ignore
    
    * LAS-7: Rename the files with UpperCase
    
    * LAS-7: Rename the files
    
    * LAS-7: Cambiando nombre de iconos.
    
    * LAS-7: Cambiando nombre de iconos.
    
    * LAS-7: Fix the image in Card Admin
    
    * LAS-7: Center the list in dashboard
    
    * LAS-7: Updat the Pills padding
    
    ---------
    
    Co-authored-by: revione <revione@outlook.com>

---
+++ src/app/dashboard/fragments/CardProperty/index.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+const labels = [
+  { src: "/icons/plus.svg", alt: "plus" },
+  { src: "/icons/user.svg", alt: "user" },
+  { src: "/icons/user.svg", alt: "dollar" },
+];
+
+export default function CardProperty() {
+  return (
+    <div className="flex gap-5 p-[10px] border rounded-md w-[401px] h-[100px]">
+      <Image
+        src="/images/img1.png"
+        alt="Picture of the author"
+        width={80}
+        height={80}
+        className="rounded-md"
+      />
+      <div className="flex flex-col gap-3">
+        <p className="font-semibold">Property address</p>
+        <div className="flex gap-5">
+          {labels.map((label, index) => (
+            <Image
+              key={`${index}-${label.alt}`}
+              src={label.src}
+              alt={label.alt}
+              width={30}
+              height={30}
+              className="bg-[#D9D9D9] p-1 rounded-full w-[30px] h-[30px]"
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,23 @@
+import CardAdmin from "./fragments/CardAdmin";
+import CardProperty from "./fragments/CardProperty";
+
+export default function Dashboard() {
+  return (
+    <>
+      <div className="flex gap-[150px] min-h-screen justify-center items-center">
+        <div className="flex flex-col gap-5 p-5 rounded-lg border max-w-[441px] h-fit">
+          {new Array(5).fill(null).map(() => (
+            <CardProperty key={Math.random() + 10} />
+          ))}
+        </div>
+        <div className="flex gap-[150px]">
+          <div className="flex flex-col gap-5 p-5 rounded-lg border max-w-[441px] h-fit">
+            {new Array(5).fill(null).map(() => (
+              <CardAdmin key={Math.random() + 10} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
