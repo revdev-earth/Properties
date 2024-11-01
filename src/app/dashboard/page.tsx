f88c43c85cb09944a2cb2eabc552b98e5b07bb4c
revione <revione@outlook.com>
2024-11-01 15:53:35 +0100

    LAS-7: Center the list in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,14 +4,14 @@ import CardProperty from "./fragments/CardProperty";
 export default function Dashboard() {
   return (
     <>
-      <div className="flex gap-[150px]">
-        <div className="flex flex-col gap-5 p-5 rounded-lg border max-w-[441px]">
+      <div className="flex gap-[150px] min-h-screen justify-center items-center">
+        <div className="flex flex-col gap-5 p-5 rounded-lg border max-w-[441px] h-fit">
           {new Array(5).fill(null).map(() => (
             <CardProperty key={Math.random() + 10} />
           ))}
         </div>
         <div className="flex gap-[150px]">
-          <div className="flex flex-col gap-5 p-5 rounded-lg border max-w-[441px]">
+          <div className="flex flex-col gap-5 p-5 rounded-lg border max-w-[441px] h-fit">
             {new Array(5).fill(null).map(() => (
               <CardAdmin key={Math.random() + 10} />
             ))}
