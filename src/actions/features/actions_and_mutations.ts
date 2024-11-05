4ba6cb20bceb1f2013e3139125486cee3b3f1ea8
Nickothan <s1nicolasrg@gmail.com>
2024-11-05 15:10:16 -0500

    LAS-10: actions and mutations and manager from features.

---
+++ src/actions/features/actions_and_mutations.ts
@@ -0,0 +1,73 @@
+"use server";
+
+import { Prisma } from "@prisma/client";
+import { featuresManager } from "./manager";
+
+export async function createFeatures({
+  data,
+}: {
+  data: Prisma.FeaturesCreateInput;
+}) {
+  try {
+    const newFeatures = await featuresManager.createFeature({
+      data,
+    });
+    return newFeatures;
+  } catch (error) {
+    console.error("Error creating property:", error);
+    throw error;
+  }
+}
+
+export async function getFeature(props?: {
+  options?: Prisma.FeaturesFindManyArgs;
+}) {
+  try {
+    const properties = await featuresManager.getFeature(props);
+    return properties;
+  } catch (error) {
+    console.error("Error fetching properties:", error);
+    throw error;
+  }
+}
+
+export async function getFeaturesById({ id }: { id: number }) {
+  try {
+    const property = await featuresManager.getFeatureById({ id });
+    return property;
+  } catch (error) {
+    console.error("Error fetching property by ID:", error);
+    throw error;
+  }
+}
+
+export async function updateFeatures({
+  id,
+  data,
+}: {
+  id: number;
+  data: Prisma.FeaturesUpdateInput;
+}) {
+  try {
+    const updatedFeatures = await featuresManager.updateFeature({
+      id,
+      data,
+    });
+    return updatedFeatures;
+  } catch (error) {
+    console.error("Error updating property:", error);
+    throw error;
+  }
+}
+
+export async function deleteFeatures({ id }: { id: number }) {
+  try {
+    const deletedFeatures = await featuresManager.deleteFeature({
+      id,
+    });
+    return deletedFeatures;
+  } catch (error) {
+    console.error("Error deleting property:", error);
+    throw error;
+  }
+}
