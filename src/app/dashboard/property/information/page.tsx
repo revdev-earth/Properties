5d0c9ad13f781134f4015e65f0ff5dc808edd389
Nickothan <s1nicolasrg@gmail.com>
2024-11-07 09:15:11 -0500

    LAS-10: model, manager, actions and read from information properties.

--- src/app/dashboard/property/information/page.tsx
+++ src/app/dashboard/property/information/page.tsx
@@ -1,31 +1,12 @@
 "use client";
 import { getInformationProperties } from " +/actions/informationProperty/actions_and_mutations";
-import {
-  Address,
-  Exterior,
-  Features,
-  InformationProperty,
-  Interior,
-} from "@prisma/client";
+import { InformationProperty } from "@prisma/client";
 import { useEffect, useState } from "react";
-
-import CardAddress from "./fragments/CardAddress";
-import CardFeatures from "./fragments/CardFeatures";
-import CardInterior from "./fragments/CardInterior";
-import CardExterior from "./fragments/CardExterior";
-import { getAddress } from " +/actions/address/actions_and_mutations";
-import { getInterior } from " +/actions/interior/actions_and_mutations";
-import { getExterior } from " +/actions/exterior/actions_and_mutations";
-import { getFeatures } from " +/actions/features/actions_and_mutations";
 
 export default function Information() {
   const [informationProperties, setInformationProperties] = useState<
     InformationProperty[]
   >([]);
-  const [address, setAddress] = useState<Address[]>([]);
-  const [interior, setInterior] = useState<Interior[]>([]);
-  const [exterior, setExterior] = useState<Exterior[]>([]);
-  const [features, setFeatures] = useState<Features[]>([]);
 
   useEffect(() => {
     async function fetchInformationProperties() {
@@ -35,64 +16,45 @@ export default function Information() {
     fetchInformationProperties();
   }, []);
 
-  useEffect(() => {
-    async function fetchAddress() {
-      const address = await getAddress();
-      setAddress(address);
-    }
-    fetchAddress();
-  }, []);
-
-  useEffect(() => {
-    async function fetchInterior() {
-      const inter = await getInterior();
-      setInterior(inter);
-    }
-    fetchInterior();
-  }, []);
-
-  useEffect(() => {
-    async function fetchExterior() {
-      const exterior = await getExterior();
-      setExterior(exterior);
-    }
-    fetchExterior();
-  }, []);
-
-  useEffect(() => {
-    async function fetchFeatures() {
-      const feature = await getFeatures();
-      setFeatures(feature);
-    }
-    fetchFeatures();
-  }, []);
-
   return (
     <>
       <div className="flex gap-[150px] min-h-screen justify-center items-center">
         <div className="flex flex-col gap-5 p-5 rounded-lg border max-w-[441px] h-fit">
-          {informationProperties.map((property) => (
-            <div key={property.id} className="border-b py-5">
-              <h3 className="font-bold text-xl mb-3">Property Details</h3>
-              {/* Address Card */}
-              {address.map((address) => {
-                return <CardAddress address={address} key={address.id} />;
-              })}
-              {/* Features Card */}
-              {features.map((feature) => {
-                return <CardFeatures features={feature} key={feature.id} />;
-              })}
+          {informationProperties.map((property) => {
+            {
+            }
+            return (
+              <div key={property.id} className="border-b py-5">
+                <h3 className="font-bold text-xl mb-3">Property Information</h3>
 
-              {/* Interior Card */}
-              {interior.map((interior) => {
-                return <CardInterior interior={interior} key={interior.id} />;
-              })}
-              {/* Exterior Card */}
-              {exterior.map((exterior) => {
-                return <CardExterior exterior={exterior} />;
-              })}
-            </div>
-          ))}
+                <p>Address number: {property.streetAndNumber}</p>
+                <p>Neighborhood: {property.neighborhood}</p>
+                <p>City and State: {property.cityAndState}</p>
+                <p>Postal code: {property.postalCode}</p>
+                <p>GPS Coordinates: {property.gpsCoordinates}</p>
+
+                <p>Property type: {property.propertyType}</p>
+                <p>Total land area: {property.totalLandArea} m²</p>
+                <p>Built area: {property.builtArea} m²</p>
+                <p>Floors: {property.floors}</p>
+                <p>Orientation: {property.orientation}</p>
+                <p>Age: {property.age} years</p>
+
+                <p>Bedrooms: {property.bedrooms}</p>
+                <p>Bathrooms: {property.bathrooms}</p>
+                <p>Half bathrooms: {property.halfBathrooms}</p>
+                <p>Kitchen description: {property.kitchen}</p>
+                <p>Living and dining description: {property.livingAndDining}</p>
+                <p>Additional rooms: {property.additionalRooms}</p>
+
+                <p>Yard or garden: {property.yardOrGarden}</p>
+                <p>Parking spots: {property.parking}</p>
+                <p>Parking location: {property.parkingLocation}</p>
+                <p>Balconies and terraces: {property.balconiesAndTerraces}</p>
+                <p>Recreational areas: {property.recreationalAreas}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
