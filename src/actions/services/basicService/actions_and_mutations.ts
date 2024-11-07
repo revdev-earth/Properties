e4bb3e07de17273e463efbb23a3e0084c6888a57
Nickothan <s1nicolasrg@gmail.com>
2024-11-07 17:48:04 -0500

    LAS-14: BasicService actions and mutations.

---
+++ src/actions/services/basicService/actions_and_mutations.ts
@@ -0,0 +1,73 @@
+"use server";
+
+import { Prisma } from "@prisma/client";
+import { basicServiceManager } from "./manager";
+
+export async function createBasicService({
+  data,
+}: {
+  data: Prisma.BasicServiceCreateInput;
+}) {
+  try {
+    const newBasicService = await basicServiceManager.createBasicService({
+      data,
+    });
+    return newBasicService;
+  } catch (error) {
+    console.error("Error creating property:", error);
+    throw error;
+  }
+}
+
+export async function getBasicServices(props?: {
+  options?: Prisma.BasicServiceFindManyArgs;
+}) {
+  try {
+    const properties = await basicServiceManager.getBasicServices(props);
+    return properties;
+  } catch (error) {
+    console.error("Error fetching properties:", error);
+    throw error;
+  }
+}
+
+export async function getBasicServiceById({ id }: { id: number }) {
+  try {
+    const property = await basicServiceManager.getBasicServiceById({ id });
+    return property;
+  } catch (error) {
+    console.error("Error fetching property by ID:", error);
+    throw error;
+  }
+}
+
+export async function updateBasicService({
+  id,
+  data,
+}: {
+  id: number;
+  data: Prisma.BasicServiceUpdateInput;
+}) {
+  try {
+    const updatedBasicService = await basicServiceManager.updateBasicService({
+      id,
+      data,
+    });
+    return updatedBasicService;
+  } catch (error) {
+    console.error("Error updating property:", error);
+    throw error;
+  }
+}
+
+export async function deleteBasicService({ id }: { id: number }) {
+  try {
+    const deletedBasicService = await basicServiceManager.deleteBasicService({
+      id,
+    });
+    return deletedBasicService;
+  } catch (error) {
+    console.error("Error deleting property:", error);
+    throw error;
+  }
+}
