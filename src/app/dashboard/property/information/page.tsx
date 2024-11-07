145c5f465302df5bef1d2cc17787d3ac14f219bb
Nickothan <s1nicolasrg@gmail.com>
2024-11-07 09:37:41 -0500

    LAS-10: style from information property page.

--- src/app/dashboard/property/information/page.tsx
+++ src/app/dashboard/property/information/page.tsx
@@ -18,40 +18,52 @@ export default function Information() {
 
   return (
     <>
-      <div className="flex gap-[150px] min-h-screen justify-center items-center">
-        <div className="flex flex-col gap-5 p-5 rounded-lg border max-w-[441px] h-fit">
-          {informationProperties.map((property) => {
+      <div className="">
+        <div className="flex gap-5   h-fit">
+          {informationProperties.map((info) => {
             {
             }
             return (
-              <div key={property.id} className="border-b py-5">
-                <h3 className="font-bold text-xl mb-3">Property Information</h3>
+              <div
+                key={info.id}
+                className="flex flex-col gap-1 border-b rounded-lg p-5  bg-slate-100"
+              >
+                <h3 className="font-bold text-xl mb-3">
+                  Property {info.id} Information
+                </h3>
 
-                <p>Address number: {property.streetAndNumber}</p>
-                <p>Neighborhood: {property.neighborhood}</p>
-                <p>City and State: {property.cityAndState}</p>
-                <p>Postal code: {property.postalCode}</p>
-                <p>GPS Coordinates: {property.gpsCoordinates}</p>
+                <div className="p-5 rounded-lg border">
+                  <p>Address number: {info.streetAndNumber}</p>
+                  <p>Neighborhood: {info.neighborhood}</p>
+                  <p>City and State: {info.cityAndState}</p>
+                  <p>Postal code: {info.postalCode}</p>
+                  <p>GPS Coordinates: {info.gpsCoordinates}</p>
+                </div>
 
-                <p>Property type: {property.propertyType}</p>
-                <p>Total land area: {property.totalLandArea} m²</p>
-                <p>Built area: {property.builtArea} m²</p>
-                <p>Floors: {property.floors}</p>
-                <p>Orientation: {property.orientation}</p>
-                <p>Age: {property.age} years</p>
+                <div className="p-5 rounded-lg border">
+                  <p>Property type: {info.propertyType}</p>
+                  <p>Total land area: {info.totalLandArea} m²</p>
+                  <p>Built area: {info.builtArea} m²</p>
+                  <p>Floors: {info.floors}</p>
+                  <p>Orientation: {info.orientation}</p>
+                  <p>Age: {info.age} years</p>
+                </div>
+                <div className="p-5 rounded-lg border">
+                  <p>Bedrooms: {info.bedrooms}</p>
+                  <p>Bathrooms: {info.bathrooms}</p>
+                  <p>Half bathrooms: {info.halfBathrooms}</p>
+                  <p>Kitchen description: {info.kitchen}</p>
+                  <p>Living and dining description: {info.livingAndDining}</p>
+                  <p>Additional rooms: {info.additionalRooms}</p>
+                </div>
 
-                <p>Bedrooms: {property.bedrooms}</p>
-                <p>Bathrooms: {property.bathrooms}</p>
-                <p>Half bathrooms: {property.halfBathrooms}</p>
-                <p>Kitchen description: {property.kitchen}</p>
-                <p>Living and dining description: {property.livingAndDining}</p>
-                <p>Additional rooms: {property.additionalRooms}</p>
-
-                <p>Yard or garden: {property.yardOrGarden}</p>
-                <p>Parking spots: {property.parking}</p>
-                <p>Parking location: {property.parkingLocation}</p>
-                <p>Balconies and terraces: {property.balconiesAndTerraces}</p>
-                <p>Recreational areas: {property.recreationalAreas}</p>
+                <div className="p-5 rounded-lg border">
+                  <p>Yard or garden: {info.yardOrGarden}</p>
+                  <p>Parking spots: {info.parking}</p>
+                  <p>Parking location: {info.parkingLocation}</p>
+                  <p>Balconies and terraces: {info.balconiesAndTerraces}</p>
+                  <p>Recreational areas: {info.recreationalAreas}</p>
+                </div>
               </div>
             );
           })}
