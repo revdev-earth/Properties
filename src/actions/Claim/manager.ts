d416dcfc0cfaf87c22dfbe9d7f830072187cb31c
Nickothan <s1nicolasrg@gmail.com>
2024-11-07 15:12:46 -0500

    LAS-13: manager from Insurance

---
+++ src/actions/Claim/manager.ts
@@ -0,0 +1,74 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
+
+export class ClaimManager {
+  prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
+
+  constructor(
+    prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>
+  ) {
+    this.prisma = prisma;
+  }
+
+  async createClaim({ data }: { data: Prisma.ClaimCreateInput }) {
+    try {
+      const newClaim = await this.prisma.claim.create({
+        data: data,
+      });
+      return newClaim;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getClaims(props?: { options?: Prisma.ClaimFindManyArgs }) {
+    try {
+      const properties = await this.prisma.claim.findMany(props?.options);
+      return properties;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getClaimById({ id }: { id: number }) {
+    try {
+      const claim = await this.prisma.claim.findUnique({
+        where: { id },
+      });
+      return claim;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateClaim({
+    id,
+    data,
+  }: {
+    id: number;
+    data: Prisma.ClaimUpdateInput;
+  }) {
+    try {
+      const updatedClaim = await this.prisma.claim.update({
+        where: { id },
+        data: data,
+      });
+      return updatedClaim;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteClaim({ id }: { id: number }) {
+    try {
+      const deletedClaim = await this.prisma.claim.delete({
+        where: { id },
+      });
+      return deletedClaim;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export const claimManager = new ClaimManager(new PrismaClient());
