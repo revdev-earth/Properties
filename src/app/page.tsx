92572dabd7b87483786f38f78807514d8acbf7b8
Nickothan <s1nicolasrg@gmail.com>
2024-11-25 09:55:21 -0500

    LAS-32: navigation home to go to dashboard.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import Link from "next/link";
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/dashboard");
+  }, [router]);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Link href="dashboard"> dashboard</Link>
-      </main>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
   );
